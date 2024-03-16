5b820aba663815c97827f423e64310e6c2780b4e
Nick Righi <nicholasrighi@gmail.com>
2024-03-16 14:30:08 -0700

    nvim: change telescope keybindings

--- nvim/lua/telescope.lua
+++ nvim/lua/telescope.lua
@@ -5,11 +5,10 @@ function M.post_setup()
 	local mappings = require("telescope.mappings")
 	local actions = require("telescope.actions")
 	local builtin = require('telescope.builtin')
-	vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-	vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-	vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-	vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-	vim.keymap.set('n', '<leader>fj', builtin.grep_string, {})
+	vim.keymap.set('n', '<leader>dd', builtin.find_files, {})
+	vim.keymap.set('n', '<leader>df', builtin.live_grep, {})
+	vim.keymap.set('n', '<leader>dh', builtin.help_tags, {})
+	vim.keymap.set('n', '<leader>dk', builtin.grep_string, {})
 	
 	-- override the default select and create new window to be the
 	-- same as the split for normal mode and tmux 
